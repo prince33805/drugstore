70ba6af3dbe41c8f24b9004c5946682e3ff6bfad
prince30887 <prince30887@gmail.com>
2022-05-18 17:23:07 +0700

    make it better

--- crm1/settings.py
+++ crm1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8wf$rf$sco#1gx7lob^s+f%tg-x9+uaewcne97+2kk-lnqdkeq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['drugstore2.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'crm1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [r'C:\Users\princ\Desktop\crm1\accounts\templetes'],
+        'DIRS': [r'C:\Users\prince\Desktop\drugstore\accounts\templetes'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- readme.md
+++ readme.md
@@ -30,3 +30,4 @@ workon (newvenv)
 http://127.0.0.1:8000/
 
 ```
+"# drugstore-live" 
